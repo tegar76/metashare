79746bd428d8e3ff9d49f16e989ca346268ceb01
Bayu Purnomo <bayupaths@gmail.com>
2022-11-01 14:47:59 +0700

    mengerjakan super admin bagian dashboard

--- application/controllers/Faker.php
+++ application/controllers/Faker.php
@@ -1,1 +1,43 @@
 <?php
+
+class Faker extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+		$this->faker = Faker\Factory::create('id_ID');
+	}
+
+	public function faker_customer()
+	{
+		$count = 5;
+		$customer = array();
+		for ($i = 0; $i < $count; $i++) {
+			$customer[] = [
+				'name'	=> $this->faker->name(),
+				'phone'	=> $this->faker->phoneNumber(),
+				'email'	=> $this->faker->email(),
+			];
+		}
+		$this->db->insert_batch('customer', $customer);
+		var_dump($customer);
+	}
+
+	public function faker_transaction()
+	{
+		$count = 4;
+		$data = array();
+		for ($i = 0; $i < $count; $i++) {
+			$data[] = [
+				'code' => strtoupper($this->faker->lexify('??')),
+				'cus_id' => $this->faker->numberBetween(100, 104),
+				'admin_id' => 11,
+				'model_id' => 122
+			];
+		}
+		$this->db->insert_batch('transaction', $data);
+		var_dump($data);
+	}
+}
--- application/controllers/SuperAdmin/Dashboard.php
+++ application/controllers/SuperAdmin/Dashboard.php
@@ -2,13 +2,55 @@
 
 class Dashboard extends CI_Controller
 {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('MasterModel', 'master', true);
+	}
+
 	public function index()
 	{
+		if (isset($_GET['code'])) {
+			$code = $_GET['code'];
+			$detail = $this->master->getDetailPenugasan($code);
+			var_dump($detail);
+			die;
+		}
+		$nomor	= 1;
+		$invite	= $this->master->getDataPenugasan();
+		$data['invite'] = array();
+		if ($invite) {
+			foreach ($invite as $row => $value) {
+				$invt['nomor'] = $nomor++;
+				$invt['code'] = $value->code;
+				$invt['date'] = date('d-m-Y H:i', strtotime($value->date)) . " WIB";
+				$invt['customer'] = $value->customer;
+				$invt['category'] = $value->category;
+				$invt['model'] = $value->model;
+				$invt['admin'] = $value->admin;
+				if ($value->desc == 0) {
+					$desc = 'Belum Dikerjakan';
+				} elseif ($value->desc == 1) {
+					$desc = 'Proses Pengerjaan';
+				} elseif ($value->desc == 2) {
+					$desc = 'Sudah Dikerjakan';
+				}
+				$invt['desc'] = $desc;
+				if ($value->status < 1) {
+					$status = 'Tidak Aktif';
+				} else {
+					$status = 'Aktif';
+				}
+				$invt['status'] = $status;
+				$new_invt[] = $invt;
+			}
+			$data['invite'] = $new_invt;
+		}
+		var_dump($data);
+		die;
 		$data['title'] = 'Dashboard Super Admin';
 		$data['content'] = 'super_admin/contents/dashboard/v_dashboard';
 		$this->load->view('super_admin/layouts/wrapper', $data, FALSE);
 	}
-
 }
-
-?>
